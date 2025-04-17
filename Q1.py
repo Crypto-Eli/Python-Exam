93ee6d76d606e759f9baf4d86371fb6d7883b762
Eliezer Timaiyo <156436326+Crypto-Eli@users.noreply.github.com>
2025-04-17 14:54:04 +0300

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,5 @@
+#hourly rate = 500 #hours worked 40 #Net salary = 17000 #purpose is to calculate and automate payroll
+def function (hourly rate.Hours worked - 15% =) #multiplying hourly_rate by hours_worked.
+
+
+
---
+++ q2.py
@@ -0,0 +1,2 @@
+# Banking program
+class def int (self, account_number,balance=0)
